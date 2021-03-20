a3810eb9d65dde238541ecb93d78cb3afc4739a3
Abhishek kumar satyam <abhishek.satyam96@gmail.com>
2021-03-20 22:32:22 +0530

    added screens

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './screens/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Ok I am working...</Text>
+      <Login />
       <StatusBar style="auto" />
     </View>
   );
