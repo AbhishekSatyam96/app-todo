2bee48acd23c3cd0674f2bdda7b560032886030f
Abhishek kumar satyam <abhishek.satyam96@gmail.com>
2021-03-21 10:48:18 +0530

    fetching data from redux

--- screens/todo.js
+++ screens/todo.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Text, AsyncStorage } from 'react-native';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-const Todo = () => {
+const Todo = (props) => {
     const getId = async() => {
         const id = await AsyncStorage.getItem('id');
         console.log("here id", id);
     }
     getId();
+    console.log("props of todo",props);
     return (
         <Text>I am todo...</Text>
     )
 }
 
-export default Todo;
+const mapStateToProps = state => {
+    return {
+        todoData: state.TodoState.todoData
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators(
+        {
+            // deleteBill
+        },
+        dispatch
+    );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
